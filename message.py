a571cbf8442d8b29d2bd93798add387d19ff7e9e
Soran Ghadri <soran.gdr.cs@gmail.com>
2021-07-19 06:20:57 +0430

    1. Put the message qubit in superposition 2. Measure out the two values to get the classical bit value by collapsing the state 3. Return the classical encoded message

--- message.py
+++ message.py
@@ -13,3 +13,17 @@ def create_message(quantum_engine='',qubit_one='', message_value = 0):
 
     # entangle the original qubit with the message qubit
     CNOT | (qubit_to_send, qubit_one)
+
+    '''
+        1 - Put the message qubit in superposition 
+        2 - Measure out the two values to get the classical bit value
+            by collapsing the state. 
+        '''
+    H | qubit_to_send
+    Measure | qubit_to_send
+    Measure | qubit_one
+
+    # The qubits are now turned into normal bits we can send through classical channels
+    classical_encoded_message = [int(qubit_to_send), int(qubit_one)]
+
+    return classical_encoded_message
