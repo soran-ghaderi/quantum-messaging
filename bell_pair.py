a949e329521cbaefb9ce7b6a60e99a3f2aebb57c
Soran Ghadri <soran.gdr.cs@gmail.com>
2021-07-21 04:44:18 +0430

    Initialize qubits for the sender and the receiver

--- bell_pair.py
+++ bell_pair.py
@@ -4,3 +4,9 @@ class Bell_pair:
     def __init__(self):
         self.qubit_one = None
         self.qubit_two = None
+
+    def create_bell_pair(self, quantum_engine):
+        # Qubit one is the sender's qubit, and will be used to create a message
+        self.qubit_one = quantum_engine.allocate_qubit()
+        # Qubit two is the receiver's qubit, and will be used to re-create the message state
+        self.qubit_two = quantum_engine.allocate_qubit()
