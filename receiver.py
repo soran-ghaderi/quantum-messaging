06a697a7d4909c94d72c7b5184d16edaed3ac6ed
Soran Ghadri <soran.gdr.cs@gmail.com>
2021-07-25 14:08:29 +0430

    Measuring the Qubit and collapsing the state down to either 1 or 0

--- receiver.py
+++ receiver.py
@@ -12,3 +12,12 @@ def message_reciever(quantum_engine, message, qubit_two):
         X | qubit_two
     if message[0] == 1:
         Z | qubit_two
+
+    '''
+    Measuring the Qubit and collapsing the state down to either 1 or 0
+    '''
+    Measure | qubit_two
+
+    quantum_engine.flush()
+
+    recieved_bit = int(qubit_two)
