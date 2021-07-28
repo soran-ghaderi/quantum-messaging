273ea33f56517f7f66b126156c7a63203f37f2df
Soran Ghadri <soran.gdr.cs@gmail.com>
2021-07-28 21:21:35 +0430

    Define a custom log-formatter for the transmitter

--- transmitter.py
+++ transmitter.py
@@ -3,3 +3,19 @@ import logging
 from receiver import send_receive
 
 
+class CustomFormatter(logging.Formatter):
+
+    grey = "\x1b[38;21m"
+    yellow = "\x1b[33;21m"
+    red = "\x1b[31;21m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
