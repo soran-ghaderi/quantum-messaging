562f4e55ce703aea6190aba7f2a40773d4feaa9e
Soran Ghadri <soran.gdr.cs@gmail.com>
2021-07-29 11:19:37 +0430

    Implementing the main function

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from projectq import MainEngine
-
+from transmitter import send_full_message
 
 if __name__ == '__main__':
 
     quantum_engine = MainEngine()
     message = input("message: ")
-
+    # message = 'This is a test message - quantum p2p messaging protocol - Soran Ghadri, 2021'
+    send_full_message(message=message, quantum_engine=quantum_engine)
